87fc7326fbfcb963ece139e4817d5cb7e6ea906a
hyeok8055 <juhyeok0123@gmail.com>
2020-09-14 20:05:56 +0900

    두번째 업로드

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
 
+const config = require('./config/key');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended : true}));
+
+//application/json
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://root:root@reactclass.5cjmt.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
 }).then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 방가방가')
+  res.send('Hello World! 방가방fasfasdfasdfasdfasdfasdfafs가  asdfsdfafsdfsdf')
 })
+
+
+app.post('/register', (req,res) => {
+
+    //회원 가입 할 떄 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({ //status 200 means success
+            success: true
+        })
+    })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- models/User.js
+++ models/User.js
@@ -29,4 +29,4 @@ const userSchema = mongoose.Schema({
 
 const User = mongoose.model('User',userSchema);
 
-module.exports = {}
+module.exports = {User}
